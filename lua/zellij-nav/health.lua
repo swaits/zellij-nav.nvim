2e6cbe6ab11c76c4a01f3ace15b88bc0c904d13d
Stephen Waits <steve@waits.net>
2024-09-03 14:19:45 -0600

    chore: update healthcheck for new API

--- lua/zellij-nav/health.lua
+++ lua/zellij-nav/health.lua
@@ -1,33 +1,17 @@
 local M = {}
 
-
-M._check = function()
-    vim.health.start("zellij-nav.nvim")
-    local version = vim.fn.system("zellij --version")
-    if vim.v.shell_error ~= 0 then
-        vim.health.error("zellij executable not found in user's path")
-    else
-        vim.health.ok("found zellij version " .. string.gsub(version, "zellij ", ""))
-    end
-end
-
-M.deprecated_check = function()
-    vim.health.report_start("zellij-nav.nvim")
-    local version = vim.fn.system("zellij --version")
-    if vim.v.shell_error ~= 0 then
-        vim.health.report_error("zellij executable not found in user's path")
-    else
-        vim.health.report_ok("found zellij version " .. string.gsub(version, "zellij ", ""))
-    end
-end
-
 M.check = function()
-    local vim_version = vim.version()
-    if vim_version.minor > 10 then
-        M._check()
-    else
-        M.deprecated_check()
-    end
+  local start = vim.health.report_start or vim.health.start
+  local report_ok = vim.health.report_ok or vim.health.ok
+  local report_error = vim.health.report_error or vim.health.error
+
+  start("zellij-nav.nvim")
+  local version = vim.fn.system("zellij --version")
+  if vim.v.shell_error ~= 0 then
+    report_error("zellij executable not found in user's path")
+  else
+    report_ok("found zellij version " .. string.gsub(version, "zellij ", ""))
+  end
 end
 
 return M
